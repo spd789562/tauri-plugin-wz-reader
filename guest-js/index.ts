ca48e20bda745231c51c6ccc4e8e6084f4dfd48e
spd789562 <leo.yicun.lin@gmail.com>
2024-05-20 16:40:19 +0800

    [add] update js api

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -5,6 +5,12 @@ export type InitOptions = {
   version?: string;
 };
 
+export interface WzNodeInfo {
+  type: string;
+  name: string;
+  has_child: boolean;
+}
+
 export async function execute() {
   return await invoke('plugin:wz-reader|execute');
 }
@@ -13,6 +19,22 @@ export async function init(options: InitOptions) {
   await invoke('plugin:wz-reader|init', options);
 }
 
-export async function get_server_url(): Promise<string> {
+export async function getServerUrl(): Promise<string> {
   return await invoke('plugin:wz-reader|get_server_url');
+}
+
+export async function parseNode(): Promise<string> {
+  return await invoke('plugin:wz-reader|parse_node');
+}
+
+export async function unparseNode(): Promise<string> {
+  return await invoke('plugin:wz-reader|unparse_node');
+}
+
+export async function getNodeInfo(): Promise<WzNodeInfo> {
+  return await invoke('plugin:wz-reader|get_node_info');
+}
+
+export async function getChildInfo(): Promise<WzNodeInfo[]> {
+  return await invoke('plugin:wz-reader|get_child_info');
 }

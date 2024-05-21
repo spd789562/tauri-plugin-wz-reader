ce26738bcf69d691b79cbf30bb2cae1e2f8088bf
spd789562 <leo.yicun.lin@gmail.com>
2024-05-21 17:15:14 +0800

    [add] special version of resolve png, it will try not to parse the node on target node when resolve outlink

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,10 +1,12 @@
 mod get_node_without_parse;
 mod image_map;
 pub mod path;
+mod png;
 mod smap;
 mod zmap;
 
 pub use get_node_without_parse::*;
 pub use image_map::*;
+pub use png::*;
 pub use smap::*;
 pub use zmap::*;
